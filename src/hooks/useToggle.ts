180823de9c5296f16b6af33bf24b7c34d0053876
Lucca Romaniello <37159767+luccaromaniello@users.noreply.github.com>
2026-07-29 20:44:22 -0300

    Update useToggle.ts

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,6 +1,7 @@
 import { useCallback, useState } from "react";
 
 /**
+ * Source: https://www.joshwcomeau.com/react/usememo-and-usecallback/#inside-generic-custom-hooks-7
  * A small custom hook for boolean state.
  * Works like `useState`, but instead of exposing a setter,
  * it returns a function that simply flips the value.
